a1c0ae57d3ab4cd5c630fedd7b9a2ee6c2d6eed2
yeahzzl <yeahzzl1018@gmail.com>
2023-11-04 00:44:30 +0900

    주석추가

--- 11번/11.js
+++ 11번/11.js
@@ -1,5 +1,6 @@
 const today = new Date();
 const hour = today.getHours();
+
 function nowTime() {
   if (hour < 10) {
     console.log("good morning");
@@ -9,4 +10,7 @@ function nowTime() {
     console.log("good evening");
   }
 }
+// console.log(today);
 nowTime(hour);
+
+// new Date()로 현재 날짜 시간정보를 가져올 수 있음!
--- 8번/8.js
+++ 8번/8.js
@@ -2,3 +2,5 @@ document.querySelector(".btn").addEventListener("click", function () {
   const promptObj = prompt("아무거나 입력하세요", "");
   alert(promptObj);
 });
+
+// prompt() 메소드는 처음접해보았음
